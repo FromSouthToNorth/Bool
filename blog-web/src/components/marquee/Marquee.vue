2e400efe200572a746aa13e1af5fa0606a8d1138
FromSouthToNorth <598050554@qq.com>
2020-07-11 12:36:09 +0800

    ...

---
+++ blog-web/src/components/marquee/Marquee.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ui shape m-dialog-shadow">
+    <div class="sides">
+      <div class="side" v-for="item in carousel.items" :key="item.id">
+        <a>
+          <img :src="item.src" @load="loadOK" alt />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Marquee",
+    data() {
+      return {
+        loadCount: 0,
+        carousel: {
+          items: [
+            { id: 1, src: 'https://202007002.oss-cn-chengdu.aliyuncs.com/images/blogHomeMarquee/20200711110957.png' },
+            { id: 2, src: 'https://202007002.oss-cn-chengdu.aliyuncs.com/images/blogHomeMarquee/20200711111019.png' }
+          ]
+        }
+      }
+    },
+    methods: {
+      loadOK() {
+        this.loadCount++;
+        if (this.loadCount === this.carousel.items.length) {
+          $(".shape").shape();
+          setInterval(() => {
+            $(".shape").shape("flip left");
+          }, 3000);
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .ui.shape {
+    width: 100%;
+    border-radius: 4px;
+  }
+  .sides .side img {
+    width: 100%;
+    border-radius: 4px;
+    height: 280px;
+  }
+  .sides .side {
+    width: 100%;
+    height: 280px;
+  }
+</style>
--- blog-web/src/views/type/Type.vue
+++ blog-web/src/views/type/Type.vue
@@ -21,65 +21,7 @@
         </div>
       </div>
 
-      <div class="ui top attached segment teal">
-        <div class="ui padded vertical segment stackable m-padded-large" >
-          <div class="ui middle aligned mobile reversed stackable grid">
-            <div class="eleven wide column">
-              <h3 class="ui header"><a href="#" class="m-black"
-                                       ></a></h3>
-              <p class="blog-text m-text" ></p>
-              <div class="ui grid">
-                <div class="row">
-                  <div class="six wide column">
-                    <div class="ui mini horizontal link list">
-                      <div class="item">
-                        <img
-                             src="https://images.unsplash.com/photo-1592786999269-63f5b72a2553?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=100"
-                             alt="" class="ui avatar image">
-                        <div class="content"><a href="#"  class="header" >小王八</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="right aligned ten wide column">
-                    <a href=""
-                       class="ui teal basic label m-padded-tiny m-opacity-mini"
-                       >Java基础</a>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="sixteen wide column m-font-color">
-                                            <span class="m-margin-lr-small">
-                                                <i class="calendar alternate icon"></i><span
-                                                    >2020-06-22</span>
-                                            </span>
-                    <span class="m-margin-lr-small">
-                                                <i class="eye icon"></i><span >66</span>
-                                            </span>
-                    <span class="m-margin-lr-small">
-                                                <i class="comment outline icon"></i><span ></span>
-                                            </span>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="column">
-                    <a href=""
-                       class="ui teal basic left pointing label m-padded-comm m-margin"
-                       >spring</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="ui five wide column">
-              <a href="#" >
-                <img
-                     src="https://images.unsplash.com/photo-1592786999269-63f5b72a2553?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=350"
-                     alt="" class="ui rounded image fluid">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <blog-list></blog-list>
 
       <div class="ui bottom attached segment grid" style="padding: 6px">
       </div>
@@ -88,8 +30,10 @@
 </template>
 
 <script>
+  import BlogList from "components/blogList/BlogList"
   export default {
-    name: "Type"
+    name: "Type",
+    components: { BlogList }
   }
 </script>
 
