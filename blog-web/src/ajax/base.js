60333fa6c273ad0ba3fcc2aa0132c26bb0e9cbb8
FromSouthToNorth <598050554@qq.com>
2020-07-23 19:54:13 +0800

    ...

--- blog-web/src/ajax/base.js
+++ blog-web/src/ajax/base.js
@@ -7,8 +7,4 @@ $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
     XMLHttpRequest.setRequestHeader("token", "MyToken");
   }
   options.url = "http://127.0.0.1:8081/" + originalOptions.url
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> d07288ff1520f1959620dbcfa31c10a5debf381f
